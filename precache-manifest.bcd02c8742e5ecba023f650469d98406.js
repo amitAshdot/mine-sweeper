7e1a9223a16764a8384a0e44c2f2348bd8580e40
Amit Ashdot <amitashdot@gmail.com>
2020-07-01 17:45:20 +0300

    Updates

---
+++ precache-manifest.bcd02c8742e5ecba023f650469d98406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3300e6e07cf5671626b74841d68f79a",
+    "url": "/https//amitAshdot.github.io/mine-sweeper/index.html"
+  },
+  {
+    "revision": "d1ecc70b5cec9ea5cfd7",
+    "url": "/https//amitAshdot.github.io/mine-sweeper/static/css/main.7fa08c0a.chunk.css"
+  },
+  {
+    "revision": "3120042ff6fc90b8ed7e",
+    "url": "/https//amitAshdot.github.io/mine-sweeper/static/js/2.1d3ea18a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/https//amitAshdot.github.io/mine-sweeper/static/js/2.1d3ea18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ecc70b5cec9ea5cfd7",
+    "url": "/https//amitAshdot.github.io/mine-sweeper/static/js/main.cf124ffa.chunk.js"
+  },
+  {
+    "revision": "137d1fc50a0f8ea5a0c1",
+    "url": "/https//amitAshdot.github.io/mine-sweeper/static/js/runtime-main.1bbec56e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/http//amitAshdot.github.io/mine-sweeper/precache-manifest.db884f607a62c83d680eb068c4a3d684.js"
+  "/https//amitAshdot.github.io/mine-sweeper/precache-manifest.bcd02c8742e5ecba023f650469d98406.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/http//amitAshdot.github.io/mine-sweeper/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//amitAshdot.github.io/mine-sweeper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
